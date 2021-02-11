4755a5c41ebe2b2e0d54ab9e853ba2522d5aea1f
Abhishek Singh Sikarwar <abhisikarwar10@gmail.com>
2021-02-11 13:59:08 +0530

    initiated spring boot project as a part of assignment for credit saison

---
+++ src/main/java/com/example/assignment_abhishek/AssignmentAbhishekApplication.java
@@ -0,0 +1,15 @@
+package com.example.assignment_abhishek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentAbhishekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentAbhishekApplication.class, args);
+		System.out.println("hello world");
+
+	}
+
+}
