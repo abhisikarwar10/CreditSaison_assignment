577ac09f902cad30fb2505520fd94508daa94a9b
Abhishek Singh Sikarwar <abhisikarwar10@gmail.com>
2021-02-13 00:55:24 +0530

    project done, yayyyyyyy

--- src/main/java/com/example/assignment_abhishek/Model/VerifyCardRequestLog.java
+++ src/main/java/com/example/assignment_abhishek/Model/VerifyCardRequestLog.java
@@ -1,2 +1,51 @@
-package com.example.assignment_abhishek.Model;public class VerifyCardRequestLog {
+package com.example.assignment_abhishek.Model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name="verify_card_requestt")
+public class VerifyCardRequestLog implements Serializable {
+
+    private static final long serialVersionUID = 6234234123423445345L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "request_time")
+    private Date requestTime = new Date();
+
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Date getRequestTime() {
+        return requestTime;
+    }
+
+    public void setRequestTime(Date requestTime) {
+        this.requestTime = requestTime;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
