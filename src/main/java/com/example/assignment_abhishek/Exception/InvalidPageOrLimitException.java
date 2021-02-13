0e39d0fed2cb145ff12965969957d582d7d2dc54
Abhishek Singh Sikarwar <abhisikarwar10@gmail.com>
2021-02-13 15:05:39 +0530

    Refactored code and included pageAndLimit exception class

---
+++ src/main/java/com/example/assignment_abhishek/Exception/InvalidPageOrLimitException.java
@@ -0,0 +1,5 @@
+package com.example.assignment_abhishek.Exception;
+
+public class PageNumberException extends Exception {
+    PageNumberException(){super("Page Number can not be less than 1");}
+}
